3908ff3e1eb74f5f925481f92e54a1af2eb07666
gunha <zxcvbnm9931@epozen.com>
2022-12-22 11:02:41 +0900

    update code

--- src/main/java/com/example/mysqlcdc/handler/BinlogEventHandler.java
+++ src/main/java/com/example/mysqlcdc/handler/BinlogEventHandler.java
@@ -5,6 +5,7 @@ import com.github.shyiko.mysql.binlog.event.Event;
 import com.github.shyiko.mysql.binlog.event.EventType;
 import com.github.shyiko.mysql.binlog.event.QueryEventData;
 import com.github.shyiko.mysql.binlog.event.RowsQueryEventData;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
@@ -13,30 +14,28 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author gunha
  * @version 0.1
  * @since 2022-12-19 오전 10:30`12
  */
+@Log4j2
 @Component("eventListener")
 public class BinlogEventHandler implements BinaryLogClient.EventListener {
-
-    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
 
     @Autowired
     BinaryLogClient binaryLogClient;
 
     @Override
     public void onEvent(Event event) {
-        if (logger.isLoggable(Level.INFO)) {
-            logger.log(Level.INFO, "## Received " + event);
+        if (log.isInfoEnabled()) {
+            log.info("## Received " + event);
         }
 
         if (event.getHeader().getEventType() == EventType.QUERY) {
             QueryEventData data = event.getData();
-            System.out.println("[CDC] " + data.toString());
+            log.info(data.toString());
         }
     }
 
